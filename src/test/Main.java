391929bb4a170b8683c2b3255ba7933cb3347de3
amine <amine.dridia@esprit.tn>
2023-03-09 01:44:57 +0100

    finale

--- src/test/Main.java
+++ src/test/Main.java
@@ -33,7 +33,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../gui/Time.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../gui/StatisticsAdmin.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
